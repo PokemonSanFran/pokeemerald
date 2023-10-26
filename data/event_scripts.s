fee21d3bcfd4aa41deb315d5b27de1f887a30599
pkmnsnfrn <pkmnsnfrn@gmail.com>
2023-10-26 16:55:38 -0700

    Split map specific functions into their own files

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1056,7 +1056,9 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
-
+    
+    // Start master_dojo
+    .include "data/scripts/restricted_sparring.inc"
 	.include "data/maps/MasterDojo_Lobby/scripts.inc"
-
 	.include "data/maps/MasterDojo_BattleRoom/scripts.inc"
+    // End master_dojo
