6ac3aac788c298973dfc0e057be4756966cd3e50
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-11-21 20:12:40 -0800

    chore: added branch tags

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1058,9 +1058,8 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
-	
-	// Start restricted_sparring
+	@ Start restricted_sparring
 	.include "data/scripts/restricted_sparring.inc"
 	.include "data/maps/MasterDojo_Lobby/scripts.inc"
 	.include "data/maps/MasterDojo_BattleRoom/scripts.inc"
-	// End restricted_sparring
+	@ End restricted_sparring
